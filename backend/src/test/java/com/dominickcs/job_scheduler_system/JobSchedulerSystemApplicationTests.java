04515876c872c3be5ef71ecb497c25befeea21f5
Dominick <dominick.smith938@gmail.com>
2026-02-14 12:33:13 -0600

    Project Backend Init

---
+++ backend/src/test/java/com/dominickcs/job_scheduler_system/JobSchedulerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominickcs.job_scheduler_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSchedulerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
